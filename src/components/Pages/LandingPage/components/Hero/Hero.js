45f8bcdb547cc7e45603a9b3c626d802f390582a
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-09-16 14:12:02 +0100

    finished the landing page design just need to make it more resoinsive

--- src/components/Pages/LandingPage/components/Hero/Hero.js
+++ src/components/Pages/LandingPage/components/Hero/Hero.js
@@ -2,91 +2,27 @@ import { HeroStyle, HeroSection,HeroSection2, HeroFirstName, HeroSecoundName, He
 
 import arrowRight from "../Images/arrow.svg"
 import arrowDown from "../Images/arrowdown.svg"
-import mainImage from "../Images/mainImage.svg"
-import { useEffect, useState } from "react";
+import {myimage} from "./svgIMG"
 
 
 const Hero = () => {
-
-  let [letr,setLetr]= useState([
-    {word:0},
-    {word:0},
-    {word:0},
-    {word:0},
-    {word:0},
-    {word:0},
-    {word:0},
-    {word:0},
-    {word:0},
-    {word:0},
-    {word:0},
-    {word:0},
-    {word:0},
-    {word:0},
-    {word:0},
-    {word:0},
-    {word:0},
-  ])
-    let [counter,setCounter]= useState(0)
-    useEffect(()=>{
-      console.log(counter)
-      
-      setTimeout(()=>{
-
-        if(counter>=16){
-          setCounter(0)
-          setLetr(letr.map((word)=>{
-            return word.word=0
-          }))
-          console.log(letr)
-        }else{
-          setCounter(counter+1)
-        }
-        
-        
-        let newArr = [...letr]; 
-        newArr[counter].word = 1; 
-        setLetr(newArr)
-      },300)
-
-    },[counter])
-
+  
   return ( 
     <HeroStyle>
       <HeroSection>
 
         <HeroFirstName>
-          <h1>
-            <span style={{opacity:letr[0].word}}>J</span>
-            <span style={{opacity:letr[1].word}}>O</span>
-            <span style={{opacity:letr[2].word}}>R</span>
-            <span style={{opacity:letr[3].word}}>D</span>
-            <span style={{opacity:letr[4].word}}>A</span>
-            <span style={{opacity:letr[5].word}}>N</span>
-          </h1>
+          <h1>JORDAN</h1>
           <img src={arrowRight} alt="" srcset="" />
         </HeroFirstName>
 
         <HeroMiddleName>
-          <h1>
-            <span style={{opacity:letr[6].word}}>E</span>
-            <span style={{opacity:letr[7].word}}>D</span>
-            <span style={{opacity:letr[8].word}}>D</span>
-            <span style={{opacity:letr[9].word}}>I</span>
-            <span style={{opacity:letr[10].word}}>E</span>
-          </h1>
+          <h1>EDDIE</h1>
           <img src={arrowRight} alt="" srcset="" />
         </HeroMiddleName>
 
         <HeroSecoundName>
-          <h1>
-            <span style={{opacity:letr[11].word}}>L</span>
-            <span style={{opacity:letr[12].word}}>I</span>
-            <span style={{opacity:letr[13].word}}>N</span>
-            <span style={{opacity:letr[14].word}}>T</span>
-            <span style={{opacity:letr[15].word}}>O</span>
-            <span style={{opacity:letr[16].word}}>N</span>
-          </h1>
+          <h1>LINTON</h1>
           <img src={arrowDown} alt="" srcset="" />
         </HeroSecoundName>
 
@@ -96,7 +32,11 @@ const Hero = () => {
 
 
       <HeroSection2 style={{flexDirection:"row"}}>
-        <img className="logoIMG" src={mainImage} width="50%" alt="" srcset="" />
+        {myimage}
+        <div className="circleContainer">
+          <div className="circle">Web developer</div>
+          
+        </div>
         <article>
           <p>
             My plan is to help companies create  their dream web applications.
--- src/components/Pages/LandingPage/components/Hero/Hero.style.js
+++ src/components/Pages/LandingPage/components/Hero/Hero.style.js
@@ -10,23 +10,21 @@ export const HeroStyle = styled.main`
   color: ${BrownColor};
   @keyframes slide1 {
   0%,
-  100% {
-    transform: translate(0, 0);
-    
-  }
-
-  50% {
-    transform: translate(0, 10px);
-  }
+  100% {transform: translate(0, 0);}
+  50% {transform: translate(0, 10px);}
+}
+@keyframes spin {
+  0% { transform: rotate(-60deg); }
+  100% { transform: rotate(60deg); }
 }
 
 @keyframes typing {
   from {
-    width: 0
-  }
-  to {
-    width: 1;
-  }
+    width: 0 ;
+    opacity:0}
+  to { 
+    width: 100%; 
+    opacity:1 }
 }
 `
 
@@ -54,13 +52,18 @@ export const HeroFirstName = styled.div`
   align-self: flex-start;
   justify-content: center;
   position: relative;
-  animation: typing 2s steps(22), blink .5s step-end infinite ;
+  
  
   & img{
     position: absolute;
     bottom:-10px;
     width: 100%;
     color: ${BrownColor};
+    animation: typing 2s steps(22), blink .5s step-end infinite ;
+  }
+  & h1 {
+    overflow: hidden;
+    animation: typing 2s steps(22), blink .5s step-end infinite ;
   }
   
   @media (min-width: 1200px) {
@@ -75,9 +78,20 @@ export const HeroMiddleName = styled.div`
   flex-direction: column;
   position: relative;
   & img{
+    width: 0%;
+    overflow: hidden;
     position: absolute;
     bottom:-10px;
-    width: 100%;
+    animation: typing 2s steps(22), blink .5s step-end infinite ;
+    animation-delay: 2s;
+    animation-fill-mode: forwards;
+  }
+  & h1 {
+    width: 0%;
+    overflow: hidden;
+    animation: typing 2s steps(22), blink .5s step-end infinite;
+    animation-delay: 2s;
+    animation-fill-mode: forwards;
   }
   @media (min-width: 1200px) {
     /* font-size: 5rem; */
@@ -91,18 +105,28 @@ export const HeroSecoundName = styled.div`
   align-self: flex-end;
   & img{
     object-fit: contain;
+    animation: slide1 1s ease-in-out infinite;
+  }
+  & h1 {
+    width: 0%;
+    overflow: hidden;
+    animation: typing 2s steps(22), blink .5s step-end infinite;
+    animation-delay: 4s;
+    animation-fill-mode: forwards;
   }
   @media (min-width: 1200px) {
     /* font-size: 5rem; */
     & img{
       max-height: 100%;
-    animation: slide1 1s ease-in-out infinite;
+    
 
     }
   }
 `
 
 export const HeroSection2 = styled.section`
+  position: relative;
+
   height:50% ;
   width: 90%;
   display: flex;
@@ -111,25 +135,83 @@ export const HeroSection2 = styled.section`
   align-items: center;
   font-size: 7.04167vw;
 
-  
+  & .circleContainer{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    height: 100px;
+    width: 100px;
+    border: thin solid ${BrownColor};
+    border-radius: 100px;
+    left: 45% ;
+    top: 25%;
+    font-size: 1rem;
+    text-align: center;
+    background-color:#FBF8F4 ;
+    & .circle{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: thin solid ${BrownColor};
+      height: 90px;
+      width: 90px;
+      font-size: 1.5rem;
+      border-radius: 100px;
+      -webkit-animation: spin 4s linear infinite ;
+      animation: spin 4s linear infinite alternate;
+      animation-delay: 6s;
+
+
+    }
+  }
 
   & article{
+    position: relative;
     align-self: flex-start;
     font-size: 2.04167vw;
     width: 40%;
     padding: 10px;
     border: thin solid ;
     margin-top: 50px;
+    &:after {
+      content: '';
+	position: absolute;
+	left: 0;
+	top: 50%;
+	width: 0;
+	height: 0;
+	border: 30px solid transparent;
+	border-right-color: ${BrownColor};
+	border-left: 0;
+	border-bottom: 0;
+	margin-top: -15px;
+	margin-left: -30px;
+}
+    & p{
+      border-right: .15em solid ${BrownColor};
+      max-height: 200px;
+      width: 0%;
+      opacity: 1;
+      overflow: hidden;
+    animation: typing 2s steps(22), blink .5s step-end infinite;
+    animation-delay: 6s;
+    animation-fill-mode: forwards;
+    white-space: wrap;
+    }
   }
   & .logoIMG{
     align-self: flex-start;
     object-fit: contain;
     max-height: 100%;
+    
   }
+  
   @media (min-width: 1200px) {
     width: 1100px;
     & article{
       font-size: 2rem;
+      
       
     } 
   }

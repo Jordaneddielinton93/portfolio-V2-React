49d9b732a247a6bfe493b1602d3564f7699008c0
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-09-21 22:26:07 +0100

    added a bunch to the about page

--- src/components/Pages/AboutMePage/AboutMe.style.js
+++ src/components/Pages/AboutMePage/AboutMe.style.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
-
+import jordIMG from "./images/chaincar.jpg"
 export const AboutPageStyle = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+  align-items: center;
 `
 
 
@@ -13,23 +14,31 @@ export const ThreeColumsIntro = styled.main`
   align-items: center;
   width: 100%;
   height: 100vh;
-  margin-top: 40px;
   & .aboutColums{
     display: flex;
     flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
     width: 31%;
     height: 100%;
-    background-color: black;
+    
     & .aboutColums__imgContainer{
       display: flex;
       align-items: center;
-      width: 95%;
+      width: 400px;
       height: 70%;
-      border-radius: 190px;
-      border:solid thick red;
-      margin:auto 0;
-      & img{
+      border-radius: 200px;
+      border:solid 12px black;
+      
+      background-image: url(${jordIMG});
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: cover;
+      & .innerborder{
         width: 100%;
+        height: 100%;
+        border: 10px solid whitesmoke;
+        border-radius: 180px;
       }
     }
   }
--- src/components/Pages/AboutMePage/AboutMePage.js
+++ src/components/Pages/AboutMePage/AboutMePage.js
@@ -1,21 +1,25 @@
 
 import { AboutPageStyle, ThreeColumsIntro } from "./AboutMe.style";
-import jordIMG from "./images/chaincar.jpg"
+
 const AboutMePage = () => {
   return ( 
     <AboutPageStyle>
+      <h3>im still learning Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis voluptas atque ipsam aperiam animi debitis, vel nihil, cum nesciunt consequuntur, ad ex tenetur. Iste eaque aliquid facere quam aut qui.</h3>
       <ThreeColumsIntro>
         <section className="aboutColums">
-
+          <h1>jordan</h1>
         </section>
 
         <section className="aboutColums">
+          <h1>About jordan</h1>
           <div className="aboutColums__imgContainer" >
-            <img srcSet={jordIMG} alt="jordan" />
+            <div className="innerborder"></div>
           </div>
+          <h1>Front End Dev</h1>
         </section>
 
         <section className="aboutColums">
+        <h1>jordan</h1>
           
         </section>
         
